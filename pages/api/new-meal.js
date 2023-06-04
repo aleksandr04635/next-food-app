4450ce4478896260c496b69c4887f8f298a4f518
aleksandr04635 <lap04635@gmail.com>
2023-06-04 13:04:58 +0300

    first commit

---
+++ pages/api/new-meal.js
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
+  const DATABASE_NAME = "FoodApp";
+  const DATABASE_PASSWORD = "1234567H";
+
+  if (req.method === "POST") {
+      const client = await MongoClient.connect(
+        `mongodb+srv://aleksandr04635:df368ie90@cluster0.vkcz0.mongodb.net/next-food-app?retryWrites=true&w=majority`
+     // `mongodb+srv://masu:${DATABASE_PASSWORD}@foodapp.0tpop.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`
+    );
+    const db = client.db();
+    const mealsCollection = db.collection("meals");
+    await mealsCollection.insertOne(req.body);
+
+    client.close();
+
+    res.status(201).send({ Message: "Meal inserted" });
+  }
+};
+
+export default handler;
