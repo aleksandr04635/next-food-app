8407610212db72639d850267b2b06effb16188a3
aleksandr04635 <lap04635@gmail.com>
2023-06-04 19:29:29 +0300

    ver3

--- next.config.js
+++ next.config.js
@@ -1,26 +1,18 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  reactStrictMode: true,
+
   async headers() {
     return [
       {
         source: "/",
+
         headers: [
           {
-            key: "cache-control",
-            value: "private, s-maxage=60, stale-while-revalidate=30",
-          },
-        ],
-      },
-      {
-        source: "/blog/:slug*",
-        headers: [
-          {
-            key: "x-slug",
-            value: ":slug*", // Matched parameters can be used in the value
-          },
-          {
-            key: "x-slug-:slug*", // Matched parameters can be used in the key
-            value: "my other custom header value",
+            key: "Cache-Control",
+
+            value: "s-maxage=1, stale-while-revalidate=59",
           },
         ],
       },
